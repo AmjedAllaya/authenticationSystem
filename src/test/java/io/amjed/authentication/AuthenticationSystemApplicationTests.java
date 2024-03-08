09f108172224bf595717871827bb77c39de0e1c8
amjed <amjed@ubuntu.myguest.virtualbox.org>
2024-03-08 18:07:51 +0100

    init project

---
+++ src/test/java/io/amjed/authentication/AuthenticationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.amjed.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
